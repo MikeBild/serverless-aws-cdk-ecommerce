8cc57cc94e18190ca45549ff06f4eb6ea1e4e213
Mike Bild <mike.bild@googlemail.com>
2019-10-08 21:00:10 +0200

    Fix e2e function name

--- packages/infrastructure/e2e-stack.js
+++ packages/infrastructure/e2e-stack.js
@@ -7,10 +7,10 @@ module.exports = class E2ETests extends Stack {
     super(parent, id, props)
 
     const { STACK_NAME, STACK_ENV, E2E_BASE_URL, E2E_USERNAME, E2E_PASSWORD } = props
-    console.log(join(__dirname, '../e2e-tests/build'))
+
     new Function(this, `${STACK_NAME}-${STACK_ENV}-E2ETests-Function`, {
-      functionName: 'e2e-tests-function',
-      runtime: Runtime.NODEJS_10_X,
+      functionName: `${STACK_NAME}-${STACK_ENV}-E2ETests-Function`,
+      runtime: Runtime.NODEJS_8_10,
       handler: 'runner.run',
       timeout: Duration.seconds(300),
       memorySize: 1024,
