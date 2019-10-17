f2658d62abd31b522efc0739221f1fba99b5535a
Mike Bild <mike.bild@googlemail.com>
2019-10-17 10:52:59 +0200

    refactor(e2e-tests): fix and active E2E setup

--- packages/e2e-tests/invoke.js
+++ packages/e2e-tests/invoke.js
@@ -1,5 +1,9 @@
 const AWS = require('aws-sdk')
-const lambda = new AWS.Lambda()
+const CDK_STACK_NAME = process.env.CDK_STACK_NAME
+const CDK_STACK_ENV = process.env.CDK_STACK_ENV
+const CDK_AWS_REGION = process.env.CDK_AWS_REGION
+
+const lambda = new AWS.Lambda({ region: CDK_AWS_REGION })
 const [_, __, count = 1] = process.argv
 
 invokeE2E(parseInt(count))
@@ -8,7 +12,7 @@ async function invokeE2E(count) {
   const lambdaInvokeList = new Array(count).fill({}).map(() =>
     lambda
       .invoke({
-        FunctionName: 'e2e-tests-function',
+        FunctionName: `${CDK_STACK_NAME}-${CDK_STACK_ENV}-E2ETests-Function`,
         InvocationType: 'RequestResponse',
         LogType: 'Tail',
         Payload: JSON.stringify({}),
@@ -19,13 +23,24 @@ async function invokeE2E(count) {
 
   try {
     const lambdaResults = await Promise.all(lambdaInvokeList)
+    console.log({ lambdaResults })
     lambdaResults.forEach(({ LogResult, Payload }) => {
+      if (LogResult.includes('Function not found:')) return console.log(LogResult)
+
       const logResults = Buffer.from(LogResult, 'base64').toString('ascii')
-      const {
-        results: { success, numFailedTests, numPassedTests, numPendingTests },
-      } = JSON.parse(Payload)
+      const { results: { success, numFailedTests, numPassedTests, numPendingTests, testResults } = {} } = JSON.parse(
+        Payload
+      )
       console.log(logResults)
-      console.log({ success, numFailedTests, numPassedTests, numPendingTests })
+      console.log('Erfolgreiche Ausführung aller Tests: ', success)
+      console.log('Erfolgreiche Tests: ', numPassedTests)
+      console.log('Nicht erfolgreiche Tests: ', numFailedTests)
+      console.log('Ausgesetzte Tests: ', numPendingTests)
+      console.log('Fehlermeldungen:')
+      testResults.forEach(x => {
+        console.log('Pfad:', x.testFilePath)
+        console.log('Fehler:', x.failureMessage)
+      })
     })
   } catch (error) {
     console.error(error)
